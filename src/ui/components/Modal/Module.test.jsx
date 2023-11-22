f98a6625ec74a59413750539bd06faf8a2d7cb57
Isela Ceballos Miranda <isela.ceballos@ikontechnologies.com>
2023-11-22 05:49:36 -0600

    Removed comments.

--- src/ui/components/Modal/Module.test.jsx
+++ src/ui/components/Modal/Module.test.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import Modal from './Modal';
 
-// Antes de las pruebas, crea un div con el ID 'modal-root' en el cuerpo del documento
+
 beforeAll(() => {
     const modalRoot = document.createElement('div');
     modalRoot.setAttribute('id', 'modal-root');
     document.body.appendChild(modalRoot);
   });
   
-  // Después de las pruebas, elimina el elemento 'modal-root' del cuerpo del documento
-  afterAll(() => {
+afterAll(() => {
     const modalRoot = document.getElementById('modal-root');
     if (modalRoot) {
       document.body.removeChild(modalRoot);
@@ -40,7 +39,7 @@ describe('Modal component', () => {
     fireEvent.click(closeButton);
     expect(onCloseMock).toHaveBeenCalled();
   });
-  
+
   it('closes modal when Escape key is pressed', () => {
     const onCloseMock = jest.fn();
     render(<Modal isOpen={true} onClose={onCloseMock} />);
