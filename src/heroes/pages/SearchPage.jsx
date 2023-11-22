1805df1dd4eaefc1cb587574d14c7887e60f183c
Isela Ceballos Miranda <isela.ceballos@ikontechnologies.com>
2023-11-22 17:50:43 -0600

    Finished the search for the fixed characters.

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -3,6 +3,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import queryString from "query-string";
 import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../components/HeroCard";
+import { getHeroesByName } from "../helpers/getHeroesByName";
 
 export const SearchPage = () => {
 
@@ -10,6 +11,8 @@ export const SearchPage = () => {
   const location = useLocation();
 
   const { q = '' } = queryString.parse(location.search);
+
+  const heroes = getHeroesByName(q);
 
   const { searchText, onInputChange } = useForm({
     searchText: ''
@@ -54,7 +57,12 @@ export const SearchPage = () => {
 
           <div className="alert alert-danger">No hero with <b>{q}</b> </div>
 
-          {/* <HeroCard/> */}
+          {
+            heroes.map( hero => (
+              <HeroCard key={ hero.id } {...hero} />
+            ))
+          }
+
         </div>
       </div>
     </>
