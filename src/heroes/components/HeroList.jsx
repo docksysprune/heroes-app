0c39d4c0e5ce37b16fee441084943606ee820894
Isela Ceballos Miranda <isela.ceballos@ikontechnologies.com>
2023-11-22 05:28:50 -0600

    Hero card.

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { getHeroesByPublisher } from '../helpers/GetHeroesByPublisher'
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
 
@@ -9,9 +10,7 @@ export const HeroList = ({ publisher }) => {
     <div className='row row-cols-1 row-cols-md-3 g-3'>
         {
             heroes.map( hero => (
-                <li key={ hero.id }>
-                    { hero.superhero }
-                </li>
+                <HeroCard key={hero.id} {...hero} />
             ))
         }
     </div>
