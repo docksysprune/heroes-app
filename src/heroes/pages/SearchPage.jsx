13a6cda9bf1f1384b79e7d6e3d10b06ec1d02fae
Isela Ceballos Miranda <isela.ceballos@ikontechnologies.com>
2023-11-22 18:08:53 -0600

    SearchPage finished.

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -14,13 +14,15 @@ export const SearchPage = () => {
 
   const heroes = getHeroesByName(q);
 
+  const showSearch = (q.length === 0);
+  const showError = (q.length > 0) && heroes.length === 0;
+
   const { searchText, onInputChange } = useForm({
-    searchText: ''
+    searchText: q
   });
 
   const onSearchSubmit = (e) => {
     e.preventDefault();
-    if ( searchText.trim().length <= 1 ) return;
 
     navigate(`?q=${ searchText.toLowerCase().trim()}`);
   }
@@ -53,9 +55,8 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">Search a hero</div>
-
-          <div className="alert alert-danger">No hero with <b>{q}</b> </div>
+          <div className="alert alert-primary" style={{ display: showSearch ? '' : 'none' }}>Search a hero</div>
+          <div className="alert alert-danger" style={{ display: showError ? '' : 'none'}}>No hero with <b>{q}</b> </div>
 
           {
             heroes.map( hero => (
